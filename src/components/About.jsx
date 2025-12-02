43e6f043347a54f3796306056f5943c21b2ae4ed
Not Sam <samuelbunker@live.com>
2025-12-01 19:37:36 -0500

    Moved the story image from above the title to below the title

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -77,12 +77,12 @@ const About = () => {
       </div>
 
       <div className="container">
+        <h2 className="section-title">THE STORY</h2>
+        <p className="section-subtitle">"Back to My Roots"</p>
+
         <div className="about-hero-image">
           <img src={aboutImage} alt="Samuel's Home" className="home-photo" />
         </div>
-
-        <h2 className="section-title">THE STORY</h2>
-        <p className="section-subtitle">"Back to My Roots"</p>
 
         <div className="about-content">
           <div className="verse">
